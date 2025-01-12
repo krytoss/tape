1fce3681cc8fac518d51dc10a327dbf05c5ea1d5
Ludovit <kopcsanyiludovit@gmail.com>
2025-01-12 14:30:57 +0100

    Added PersonCard component

---
+++ src/components/PersonCard.tsx
@@ -0,0 +1,23 @@
+type Props = {
+	img: string,
+	name: string,
+	position: string
+}
+
+const PersonCard: React.FC<Props> = ({ img, name, position}) => {
+
+
+	return (
+		<div className="person-card bg-white/50 h-auto pt-20 px-10 shadow-xl">
+			<div className="w-full h-40 bg-blue-500 overflow-visible relative text-center">
+				<img src={img} className="w-4/5 object-cover absolute bottom-0 left-1/2 transform -translate-x-1/2" />
+			</div>
+			<div className="p-4 text-center">
+				<h3 className="text-xl font-semibold">{name}</h3>
+				<p className="text-lg">{position}</p>
+			</div>
+		</div>
+	)
+}
+
+export default PersonCard
