7069b6a1d964970b0bd994a351ef98cb4fd293f6
Ludovit <kopcsanyiludovit@gmail.com>
2025-01-16 12:53:26 +0100

    Added Cover component

---
+++ src/components/Layout/Cover.tsx
@@ -0,0 +1,20 @@
+type Props = {
+	children: React.ReactNode,
+	className?: string,
+	center?: boolean,
+}
+
+const Cover: React.FC<Props> = ({ children, className, center = true }) => {
+	return (
+		<div className={`
+			absolute left-0 top-0 right-0 bottom-0
+			bg-stone-800 bg-opacity-80
+			${className ?? ""}
+			${center ? "flex items-center justify-center" : ""}
+		`}>
+			{children}
+		</div>
+	)
+}
+
+export default Cover
