08c3b0a2242bd6aa30c7aa332f7cd470eebd5a98
Ludovit <kopcsanyiludovit@gmail.com>
2025-01-16 12:52:15 +0100

    Adjustments on Title component

--- src/components/Layout/Title.tsx
+++ src/components/Layout/Title.tsx
@@ -1,14 +1,16 @@
 type Props = {
 	children: React.ReactNode,
 	color: "light" | "dark",
+	className?: string,
+	size?: string
 }
 
-const Title: React.FC<Props> = ({ children, color }) => {
+const Title: React.FC<Props> = ({ children, color, className, size = "8xl" }) => {
 
-	const className = color === "light" ? "text-white" : "text-slate-700";
+	const colorClass = color === "light" ? "text-white" : "text-slate-700";
 
 	return (
-		<h3 className={`text-8xl uppercase font-semibold block ${className}`}>
+		<h3 className={`text-${size} uppercase font-semibold block max-w-full ${className ?? ""} ${colorClass ?? ""}`}>
 			{ children }
 		</h3>
 	)
