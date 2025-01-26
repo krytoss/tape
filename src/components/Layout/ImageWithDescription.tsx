67ed0dbf0d47c0b2951d50128270514ceb9af6b4
Ludovit <kopcsanyiludovit@gmail.com>
2025-01-26 14:44:10 +0100

    Reworked image with description

--- src/components/Layout/ImageWithDescription.tsx
+++ src/components/Layout/ImageWithDescription.tsx
@@ -5,6 +5,7 @@ type Props = {
   image: string;
   description: string;
   className?: string;
+  height?: string | number; // Pridanie výšky ako prop
 };
 
 const ImageWithDescription: React.FC<Props> = ({
@@ -12,24 +13,28 @@ const ImageWithDescription: React.FC<Props> = ({
   image,
   description,
   className = "",
+  height = "200px",
 }) => (
   <div
-    className={`relative group overflow-hidden rounded-lg shadow-lg ${className}`}
+    className={`relative group overflow-hidden rounded-lg shadow-lg inline-block ${className}`}
+    style={{ height }}
   >
-    <img
-      src={image}
-      alt={description}
-      title={title}
-      className="w-full h-auto object-cover"
-    />
+    <div className="absolute inset-0">
+      <img
+        src={image}
+        alt={description}
+        title={title}
+        className="w-full h-full object-cover"
+      />
+    </div>
 
-    <div className="bg-brand text-white text-center py-2">
+    <div className="absolute bottom-0 left-0 right-0 bg-brand text-white text-center py-2 z-10">
       <h2 className="text-lg font-semibold">{title}</h2>
     </div>
 
     <div
       className="absolute inset-0 bg-brand text-white flex items-center justify-center text-center p-4 opacity-0 
-        group-hover:opacity-100 transition-opacity duration-300"
+        group-hover:opacity-100 transition-opacity duration-300 z-20"
     >
       <p className="text-md font-semibold">{description}</p>
     </div>
