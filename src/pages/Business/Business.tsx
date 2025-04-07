1dc0865fda7befb5d3f657a822b0a78d3195ca59
Ludovit <kopcsanyiludovit@gmail.com>
2025-04-07 08:45:56 +0200

    Removed logo from background

--- src/pages/Business/Business.tsx
+++ src/pages/Business/Business.tsx
@@ -17,18 +17,6 @@ const Business: React.FC = () => {
 				</Cover>
 			</Header>
 			<Section fullHeight={false} className="px-5 md:px-10 lg:px-20 xl:px-40 py-20 overflow-visible">
-				<svg
-					xmlns="http://www.w3.org/2000/svg"
-					viewBox="-100 -100 1093 1093"
-					className="absolute right-0 top-0 transform -translate-y-1/2 translate-x-1/2 z-10 opacity-80 blur-sm"
-				>
-					<path
-						id="background"
-						d="M 351.500 94.452 C 34.798 179.528, 0.533 188.925, 0.834 190.621 C 1.620 195.045, 188.309 891.565, 188.875 892.186 C 189.219 892.563, 347.675 850.431, 541 798.557 C 855.064 714.288, 892.477 704.044, 892.283 702.371 C 891.728 697.569, 704.185 0.572, 703.398 0.381 C 702.904 0.261, 544.550 42.593, 351.500 94.452"
-						fill="#1c4484"
-						fillRule="evenodd"
-					/>
-				</svg>
 				<div className="relative z-20 flex flex-col gap-6 md:gap-12">
 					<ProductCardWide
 						title="Karty"
