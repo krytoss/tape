11efded6f99f2273dff548ac2e5f96fcbc76aee7
Ludovit <kopcsanyiludovit@gmail.com>
2025-04-14 10:58:11 +0200

    Added all texts to business

--- src/components/Layout/ProductCardWide.tsx
+++ src/components/Layout/ProductCardWide.tsx
@@ -3,7 +3,7 @@ import { Link } from "react-router";
 type Props = {
 	className?: string;
 	title: string;
-	description: string;
+	description: string | string[];
 	image: string;
 	link: string;
 	invert?: boolean;
@@ -14,11 +14,19 @@ const ProductCardWide: React.FC<Props> = ({ className, title, description, image
 		<Link to={link} className="">
 			<div className={`w-full grid grid-cols-1 sm:grid-cols-10 shadow-2xl rounded-xl overflow-hidden relative transform hover:-translate-x-2 transition-transform ${className ?? ""} bg-white`}>
 				<div className={`sm:col-span-3 ${invert ? "sm:order-2" : ""}`}>
-					<img src={image} alt={title} className="w-full h-full max-h-60 object-cover object-center"/>
+					<img src={image} alt={title} className="w-full h-full object-cover object-center"/>
 				</div>
 				<div className={`sm:col-span-7 py-2 px-10 flex flex-col justify-center ${invert ? "order-1" : ""}`}>
 					<h3 className="text-xl font-semibold mb-2">{title}</h3>
-					<p>{description}</p>
+					<p>
+						{
+							Array.isArray(description) ?
+								description.map((desc, index) => (
+									<span key={index} className="block text-gray-600 mb-2">{desc}</span>
+								)) :
+								<span className="block text-gray-600">{description}</span>
+						}
+					</p>
 				</div>
 			</div>
 		</Link>
--- src/pages/Business/Business.tsx
+++ src/pages/Business/Business.tsx
@@ -20,14 +20,24 @@ const Business: React.FC = () => {
 				<div className="relative z-20 flex flex-col gap-6 md:gap-12">
 					<ProductCardWide
 						title="Karty"
-						description="Vstupné karty s plnofarebnou potlačou pre zamestnancov a návštevníkov. Ideálne pre budovy, haly či konferencie – kompatibilné so všetkými čítačkami."
+						description={[
+							"Ponúkame širokú škálu vstupných kariet vďaka možnosti plnofarebnej potlače loga, mena zamestnanca, fotografie a iných dôležitých informácií. Naše karty sú kompatibilné so všetkými bežnými čítačkami a zabezpečovacími systémami.",
+							"S personalizovanými vstupnými kartami môžete ľahko sledovať a kontrolovať pohyb zamestnancov, čím zvýšite úroveň bezpečnosti a ochrany vašich priestorov.",
+							"Ideálne riešenie pre administratívne budovy, výrobné haly, sklady či konferenčné priestory."
+						]}
 						image="/pages/business/karty.png"
 						link="/zabava/karty"
 					/>
 
 					<ProductCardWide
 						title="Šnúrky na krk"
-						description="Elegantné saténové aj pevné textilné šnúrky na karty a kľúče s možnosťou potlače. Ideálne na každodenné použitie aj eventy."
+						description={[
+							"Šnúrky na krk sú skvelým riešením pre zlepšenie organizácie a bezpečnosti vo vašej firme.",
+							"Poskytnite svojim zamestnancom, návštevníkom a obchodným partnerom šnúrky na krk s identifikačnými kartami, kľúčmi alebo bezpečnostnými prístupovými kartami.",
+							"V ponuke štýlové saténové aj odolné textilné varianty, všetko s možnosťou plnofarebnej potlače alebo bez potlače.",
+							"Posilnite firemnú identitu a zvýšte povedomie o vašej značke pomocou šnúrok na krk s vaším logom a farbami.",
+							"Ideálne pre konferencie, obchodné stretnutia, veľtrhy alebo každodenné pracovné prostredie.",
+						]}
 						image="/pages/business/snurky.png"
 						link="/zabava/snurky"
 						invert={true}
@@ -35,14 +45,32 @@ const Business: React.FC = () => {
 
 					<ProductCardWide
 						title="Puzdrá, rolery, menovky"
-						description="Chráňte a prezentujte identifikačné karty profesionálne. Visačky, rolery a menovky z kvalitných materiálov pre firmy a eventy."
+						description={[
+							"Chráňte a prezentujte identifikačné karty vašich zamestnancov, návštevníkov a obchodných partnerov s našimi kvalitnými plastovými visačkami a puzdrami.",
+							"Sú výborným doplnkom ku šnúrkam na krk alebo kartám.",
+							"Dôležité identifikačné údaje tak budú vždy viditeľné a chránené.",
+							"Menovky sú neoceniteľným nástrojom na budovanie vzťahov a posilnenie komunikácie vo vašej firme.",
+							"Umožňujú rýchlu a jednoduchú identifikáciu, čo prispieva k ľahšiemu nadväzovaniu kontaktov a zvýšeniu bezpečnosti v pracovnom prostredí.",
+							"Rolery sú praktickým držiakom na visačky s identifikačnými kartami, ktoré možno umiestniť kdekoľvek na oblečení.",
+							"Poskytujú pohodlie a flexibilitu pri používaní identifikačných kariet počas pracovného dňa.",
+							"V ponuke rôzne veľkosti, materiály a prevedenia, ktoré sa prispôsobia vašim potrebám a firemnému štýlu.",
+							"Zabezpečte profesionálny a efektívny systém identifikácie vo vašej firme."
+						]}
 						image="/pages/business/puzdra_rolery_menovky.png"
 						link="/zabava/puzdra-menovky-rolery"
 					/>
 
 					<ProductCardWide
 						title="RFID produkty"
-						description="Bezkontaktné riešenia: RFID karty, náramky a kľúčenky pre dochádzku, vstupy a stravovacie systémy. Možnosť personalizácie."
+						description={[
+							"Naša ponuka RFID produktov vrátane čipových kariet, látkových a silikónových náramkov a kľúčeniek zabezpečí bezproblémový a bezpečný systém pre vašu firmu.",
+							"RFID riešenia umožňujú bezkontaktné transakcie, čím zvyšujú efektivitu a pohodlie vo vašom pracovnom prostredí.",
+							"S našimi RFID riešeniami môžete zlepšiť bezpečnosť a kontrolu prístupu vo vašej firme.",
+							"RFID karty a náramky umožňujú jednoduchý a rýchly prístup do firemných priestorov, monitorovanie pohybu zamestnancov a návštevníkov, a zlepšujú celkovú organizáciu.",
+							"RFID produkty sú ideálne pre využitie v dochádzkových systémoch, stravovacích kartách a systémoch oprávnenia vstupu.",
+							"Naše RFID produkty sú ideálne pre administratívne budovy, sklady, výrobné haly a konferenčné centrá.",
+							"Vďaka možnosti plnofarebnej potlače môžete personalizovať karty a náramky s vaším logom a ďalšími dôležitými informáciami."
+						]}
 						image="/pages/business/rfid.png"
 						link="/zabava/rfid"
 						invert={true}
@@ -50,7 +78,15 @@ const Business: React.FC = () => {
 
 					<ProductCardWide
 						title="Tlačiarne a QR/čiarové kódy"
-						description="Tlač čiarových a QR kódov priamo u vás. Efektívne riešenie pre evidenciu, vstupy a ochranu zamestnancov aj návštevníkov."
+						description={[
+							"Vytlačte čiarové kódy, texty, logá a jedinečné variabilné údaje na každý identifikátor priamo vo firme.",
+							"Naše tlačiarne umožňujú tlač čiarových kódov a QR kódov, ktoré zabezpečia rýchlu a spoľahlivú identifikáciu vašich zamestnancov a návštevníkov.",
+							"Naše riešenia sú ideálne pre rôzne firemné aplikácie vrátane dochádzkových systémov, stravovacích kariet a oprávnení vstupu.",
+							"Začať s čiarovými kódmi a QR kódmi je jednoduchšie, než si myslíte.",
+							"Dokonca aj malé organizácie môžu profitovať z ich používania vďaka nízkym počiatočným nákladom.",
+							"Používaním čiarových kódov a QR kódov zabezpečíte rýchlu a spoľahlivú identifikáciu zamestnancov a ochranu pred falšovaním.",
+							"Naše tlačiarne a čiarové/QR kódy sú ideálne pre administratívne budovy, sklady, výrobné haly a konferenčné centrá."
+						]}
 						image="/pages/business/tlaciarne.png"
 						link="/zabava/tlaciarne"
 					/>
